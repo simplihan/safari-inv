4d1f4a9c0a552037df750ab5ee17e79839dd20fe
Harendra Kumar <harendra@simplihan.com>
2026-06-06 11:50:18 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
+    cloudflare(),
     tanstackStart(),
     react(),
   ],
