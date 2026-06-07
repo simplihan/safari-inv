8d1740a414cc3811f8a04f284974231cd4ba4e7c
Harendra Kumar <harendra@simplihan.com>
2026-06-07 11:51:54 +0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    tsconfigPaths(),
+    tanstackStart(),
+    nitro({
+      preset: "vercel",
+      output: {
+        dir: "dist",
+      },
+    }),
+    react(),
+  ],
+  css: {
+    transformer: "lightningcss",
+  },
+  resolve: {
+    dedupe: [
+      "react",
+      "react-dom",
+      "react/jsx-runtime",
+      "react/jsx-dev-runtime",
+      "@tanstack/react-query",
+      "@tanstack/query-core",
+    ],
+  },
+});
